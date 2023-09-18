90a0767e50c7232c6f37321c4075b3f9cf31d4a0
biswasnandini <114243527+biswasnandini@users.noreply.github.com>
2023-09-18 16:00:18 +0530

    DAY-1.cpp

--- DAY1.cpp
+++ DAY1.cpp
@@ -1,36 +1,18 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    // your code goes here
-    int t;
-    cin>>t;
-    for(int i=0;i<t;i++){
-        int num;
-        cin>>num;
-        int size=1000,nfact[size],carry=0,j=size-1;
-        nfact[size-1]=1;
-        while(num>1)
-        {
-            int x;
-            for(int k=size-1;k>=j;k--)
-            {
-                x=nfact[k]*num + carry;
-                nfact[k]=x%10;
-                carry=x/10;
-            }
-            while(carry>0)
-            {
-                nfact[--j]=carry%10;
-                carry/=10;
-            }
-            num--;
-        }
-            for(int k=j;k<size;k++){
-                cout<<nfact[k];
-            }
-            cout<<endl;
+int main() {
+int t;
+cin>>t;
+while(t--){
+    int n;
+    cin>>n;
+    if(n>=2000){
+        cout<<"YES"<<endl;
     }
-    return 0;
+    else{
+        cout<<"NO"<<endl;
+    }
+}
+	return 0;
 }
