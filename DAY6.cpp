0a7ce48f3047643d46dc3ef4ec502f13838a7dbe
biswasnandini <114243527+biswasnandini@users.noreply.github.com>
2023-09-23 18:30:55 +0530

    DAY-6.cpp

---
+++ DAY6.cpp
@@ -0,0 +1,32 @@
+//Rearranging digits to get a multiple of 5
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int d;
+	    cin>>d;
+	    string n;
+	    cin>>n;
+	    int flag=0;
+	   for(int i=0;i<d;i++)
+	   {
+	       if(n[i]=='0' || n[i]=='5')
+	       {
+	           cout<<"Yes"<<endl;
+	           flag=1;
+	           break;
+	       }
+	   }
+	   if(flag==0)
+	   {
+	       cout<<"No"<<endl;
+	   }
+	   
+	}
+	return 0;
+}
