44c64693a0b711cf4d1d22e1ec851382be7c274d
biswasnandini <114243527+biswasnandini@users.noreply.github.com>
2023-09-29 20:39:36 +0530

    DAY-12.cpp

--- DAY12.cpp
+++ DAY12.cpp
@@ -1,62 +1,28 @@
-//bear and candies 123
-#include <iostream>
+//mario and bullet
+#include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-	// your code goes here
-	  // * Initializing variables
-    int numberOfTestCases, limakMax, bobMax, limakTotal, bobTotal;
+signed main()
+{
 
-    // * Accepting the number of test cases
-    cin>>numberOfTestCases;
-
-    // * Executing each test case one by one
-    while(numberOfTestCases--) {
-
-        // * Accepting maximum possible candies limak and bob can eat
-        cin>>limakMax>>bobMax;
-
-        // * Initializing total candies with limak and bob to 0 for current test case
-        limakTotal = 0;
-        bobTotal = 0;
-
-        // * Initializing the counter to 1
-        int i = 1;
-
-        /*
-        *   Running an infinite loop, we'll break it in between
-        *   when we get a solution
-        */
-        while(1) {
-
-            /*
-            *   If i is odd, increment total number of candies with limak  by i
-            *   Otherwise, if i is even, increment total number of candies with bob by i
-            */
-            if(i&1) {
-                limakTotal += i;
-            } else {
-                bobTotal += i;
-            }
-
-            /*
-            *   If total number of candies with limak is more than the maximum he can eat,
-            *   Bob Wins. Otherwise, if the total number of candies with bob is more than
-            *   the maximum he can eat, Limak Wins.
-            *   Break the loop if anyone of limak or bob wins.
-            */
-            if(limakTotal > limakMax) {
-                cout<<"Bob"<<endl;
-                break;
-            }
-            else if(bobTotal > bobMax) {
-                cout<<"Limak"<<endl;
-                break;
-            }
-
-            // * Incrementing the counter by 1
-            i++;
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    cout << fixed << setprecision(12);
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        int x;
+        int y;
+        int z;
+        cin>>x>>y>>z;
+        int time=y/x;
+        if(z<time)
+        {
+            cout<<0<<endl;
         }
-    }
-	return 0;
+        else{
+            cout<<z-time<<endl;
+        }
+            }
+    return 0;
 }
