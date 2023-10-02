92c1f18e797262a3cac7f3b521f670260a13edce
biswasnandini <114243527+biswasnandini@users.noreply.github.com>
2023-10-02 19:35:58 +0530

    DAY-15.cpp

---
+++ DAY15.cpp
@@ -0,0 +1,34 @@
+//Flip the cards
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n,x;
+	    cin>>n>>x;
+	    if( x == 0)
+	    {
+	        cout<<0<<endl;
+	    }
+	    else if( x == n)
+	    {
+	        cout<<0<<endl;
+	    }
+	    else
+	    {
+	        if(n/2 >= x)
+	        {
+	            cout<<x<<endl;
+	        }
+	        else
+	        {
+	            cout<<n-x<<endl;
+	        }
+	    }
+	}
+	return 0;
+}
