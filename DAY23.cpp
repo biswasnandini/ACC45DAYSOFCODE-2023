8b5f7a4adc3c6ef30d1ccc29af2f9abb580d755a
biswasnandini <114243527+biswasnandini@users.noreply.github.com>
2023-10-11 22:41:28 +0530

    DAY-23.cpp

---
+++ DAY23.cpp
@@ -0,0 +1,16 @@
+//richie rich
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    for(int i=0;i<t;i++)
+    {
+        int a,b,x;
+        cin>>a>>b>>x;
+        int z=(b-a)/x;
+        cout<<z<<endl;
+    }
+    return 0;
+}
