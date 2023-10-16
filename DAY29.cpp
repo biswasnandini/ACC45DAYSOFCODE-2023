635595bd200d569bff66c970e5ac2f0bf5210c6a
biswasnandini <114243527+biswasnandini@users.noreply.github.com>
2023-10-16 21:46:45 +0530

    DAY-29.cpp

---
+++ DAY29.cpp
@@ -0,0 +1,23 @@
+//Netflix
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int a,b,c,n;
+	    cin>>a>>b>>c>>n;
+	    if(a+b >=n || b+c >=n || c+a >= n)
+	    {
+	        cout<<"YES"<<endl;
+	    }
+	    else
+	    {
+	        cout<<"NO"<<endl;
+	    }
+	}
+	return 0;
+}
