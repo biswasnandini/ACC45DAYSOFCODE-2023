cd7c389fc6bfb23a387fca3a28b02da81937d366
biswasnandini <114243527+biswasnandini@users.noreply.github.com>
2023-10-23 00:09:37 +0530

    DAY-35.cpp

---
+++ DAY35.cpp
@@ -0,0 +1,21 @@
+// C++ - Weights
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, w, x, y, z;
+	cin >> t;
+	while(t--) {
+	    cin >> w >> x >> y >> z;
+	    if ( w == (x + y + z)) {
+	        cout << "YES" << endl;
+	    } else if ( w == (x + y) || w == (x + z) || w == (y + z) ) {
+	        cout << "YES" << endl;
+	    } else if ( w == x || w == y || w == z ) {
+	        cout << "YES" << endl;
+	    } else {
+	        cout << "NO" << endl;
+	    }
+	}
+	return 0;
+}
