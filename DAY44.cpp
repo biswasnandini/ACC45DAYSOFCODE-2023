13dd8d4228f3748108ed39350c1937e240181011
biswasnandini <114243527+biswasnandini@users.noreply.github.com>
2023-10-31 21:37:21 +0530

    DAY-44.cpp

---
+++ DAY44.cpp
@@ -0,0 +1,12 @@
+//Watching movie at 2x
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x,y,ans;
+	cin>>x>>y;
+	ans = (x-y) + y/2;
+	cout<<ans;
+	return 0;
+}
