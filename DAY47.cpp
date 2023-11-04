8405e8ff485d3c88ec35160f8a821f752677021f
biswasnandini <114243527+biswasnandini@users.noreply.github.com>
2023-11-04 23:18:12 +0530

    DAY-47.cpp

---
+++ DAY47.cpp
@@ -0,0 +1,17 @@
+//Chairs Requirements
+#include <iostream>
+using namespace std;
+int n,x,y;
+int main() {
+	// your code goes here
+	cin>>n;
+    while (n--)
+    {
+        cin>>x>>y;
+        if (x<=y) 
+            cout<<0<<endl;
+        else 
+            cout<<x-y<<endl;
+    }
+	return 0;
+}
