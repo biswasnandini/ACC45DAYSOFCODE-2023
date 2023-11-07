897303d1e132805d85abdc063ec4942254415b58
biswasnandini <114243527+biswasnandini@users.noreply.github.com>
2023-11-07 22:58:49 +0530

    DAY-49.cpp

---
+++ DAY49.cpp
@@ -0,0 +1,23 @@
+//Chess ratings
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int x,y;
+	    cin>>x>>y;
+	    if((y-x)%8 == 0)
+	    {
+	        cout<<(y-x)/8<<endl;
+	    }
+	    else
+	    {
+	        cout<<((y-x)/8)+1<<endl;
+	    }
+	}
+	return 0;
+}
